09db57bf84798912a071cc21b062aca7e35fc88a
fuldenbeytur <fulden.beytur@gmail.com>
2019-07-26 19:10:16 +0100

    Updates

---
+++ precache-manifest.958471f20801f5256b04c105e9563b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde1f05ae53ef9f58d0694de1d3d2315",
+    "url": "/QuotesWebsite/index.html"
+  },
+  {
+    "revision": "ae7f4056f91229bd5982",
+    "url": "/QuotesWebsite/static/css/main.cafc5289.chunk.css"
+  },
+  {
+    "revision": "c3961942ccf50813dfcc",
+    "url": "/QuotesWebsite/static/js/2.f79bec37.chunk.js"
+  },
+  {
+    "revision": "ae7f4056f91229bd5982",
+    "url": "/QuotesWebsite/static/js/main.390cdf41.chunk.js"
+  },
+  {
+    "revision": "4f27adcbb8822d1c4e7c",
+    "url": "/QuotesWebsite/static/js/runtime~main.d39edf85.js"
+  },
+  {
+    "revision": "e37f00421a88e61f7c87c24535203c07",
+    "url": "/QuotesWebsite/static/media/quotehomepage4.e37f0042.jpg"
+  }
+]);
