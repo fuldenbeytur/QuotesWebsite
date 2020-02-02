5324cf63967222021cbf5077ca9b89ff79009317
fuldenbeytur <fulden.beytur@gmail.com>
2020-02-02 16:07:53 +0000

    test

---
+++ build/precache-manifest.ac089fc962bea98b833cd38a411c1de3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de930ad6da8df7e36f0a1649ec9f39b",
+    "url": "/QuotesWebsite/index.html"
+  },
+  {
+    "revision": "5ef1b70cded17cbca684",
+    "url": "/QuotesWebsite/static/css/main.cafc5289.chunk.css"
+  },
+  {
+    "revision": "c3961942ccf50813dfcc",
+    "url": "/QuotesWebsite/static/js/2.f79bec37.chunk.js"
+  },
+  {
+    "revision": "5ef1b70cded17cbca684",
+    "url": "/QuotesWebsite/static/js/main.9dd6a18c.chunk.js"
+  },
+  {
+    "revision": "4f27adcbb8822d1c4e7c",
+    "url": "/QuotesWebsite/static/js/runtime~main.d39edf85.js"
+  },
+  {
+    "revision": "e37f00421a88e61f7c87c24535203c07",
+    "url": "/QuotesWebsite/static/media/quotehomepage4.e37f0042.jpg"
+  }
+]);
